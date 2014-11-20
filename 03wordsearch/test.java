485352cbc815146283b82526f73f263388e88c3b
Yubin <yubinkim57@gmail.com>
2014-11-20 12:22:33 -0500

    AddWordHorizontal

--- 03wordsearch/test.java
+++ 03wordsearch/test.java
@@ -1,10 +1,12 @@
 public class test{
 
     public static void main(String [] args){
-	WordGrid  a = new WordGrid(3, 3);
+	WordGrid  a = new WordGrid(7, 7);
 	System.out.print(a.toString());
-
-
+	System.out.println(a.addWordHorizontal("YUBIN", 3, 4));
+	System.out.println("should be false");
+	System.out.println(a.addWordHorizontal("YURA", 2, 3));
+	System.out.println("should be true");
 
     }
 
