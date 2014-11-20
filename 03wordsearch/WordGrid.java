507a8681e204f7355a2251b162d8852314456e97
Yubin <yubinkim57@gmail.com>
2014-11-19 21:22:27 -0500

    WordGrid

---
+++ 03wordsearch/WordGrid.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class WordGrid{
+
+    private char[][] data;
+
+    public WordGrid(int row, int col){
+	data = new char[row][col];
+
+    }
+
+
+    public void clear(){
+	
+
+    }
+
+
+    
+
+}
